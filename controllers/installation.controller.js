6ae510867d88942ee0e7d04bd0bf80f0e2a97171
Umair <30388791+mtahirofficial@users.noreply.github.com>
2024-11-17 11:15:05 +0500

    Email on Installation

--- controllers/installation.controller.js
+++ controllers/installation.controller.js
@@ -111,14 +111,16 @@ class InstallationController extends Controller {
                 let renderPath = "home"
                 let redirectPath = `https://admin.shopify.com/store/${shop.replace(".myshopify.com", '')}/apps/${APP_PATH}/${renderPath + query}`
 
-                await MailerService.sendEmail({
-                    to: shopData?.email || shopData?.customer_email,
-                    subject: `Welcome to ${process.env.APP_NAME}`,
-                    template: "welcomeToApp",
-                    context: {
-                        appName: process.env.APP_NAME
-                    },
-                });
+                if (process.env.NODE_ENV !== "development") {
+                    await MailerService.sendEmail({
+                        to: shopData?.email || shopData?.customer_email,
+                        subject: `Welcome to ${process.env.APP_NAME}`,
+                        template: "welcomeToApp",
+                        context: {
+                            appName: process.env.APP_NAME
+                        },
+                    });
+                }
 
                 res.redirect(redirectPath)
             } else {
