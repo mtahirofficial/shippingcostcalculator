8df0c9d035e4163d6a37233bf82bdb0582eedd50
Tahir <30388791+mtahirofficial@users.noreply.github.com>
2025-07-16 01:08:51 +0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -5,6 +5,8 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
+
+console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
