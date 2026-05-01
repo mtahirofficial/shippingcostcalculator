4fad5526bf9cd8dc7d614c3b31439c3a7829d120
Muhammad Tahir <30388791+mtahirofficial@users.noreply.github.com>
2026-05-01 22:48:36 +0500

    build upload

---
+++ build/assets/UndoIcon.svg-me3erXoi.js
@@ -0,0 +1,1 @@
+import{R as a}from"./index-CPH87iC1.js";var n=function(e){return a.createElement("svg",Object.assign({viewBox:"0 0 20 20"},e),a.createElement("path",{d:"M7.47 3.72a.75.75 0 0 1 1.06 1.06l-1.72 1.72h3.94a5 5 0 0 1 0 10h-1.5a.75.75 0 0 1 0-1.5h1.5a3.5 3.5 0 1 0 0-7h-3.94l1.72 1.72a.75.75 0 1 1-1.06 1.06l-3-3a.75.75 0 0 1 0-1.06l3-3Z"}))};n.displayName="UndoIcon";export{n as S};
